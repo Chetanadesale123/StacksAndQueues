3f24b1dab074c2bd1e9a3fddfa74b14e08a680aa
59114_Chetana <deslechetna1999@gmail.com>
2022-05-03 18:03:02 +0530

    [Chetana]Added:Creat Stacks by calling push function

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StacksAndQueues;
+using System;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Stacks stack = new Stacks();
+        stack.Push(56);
+        stack.Push(30);
+        stack.Push(70);
+        stack.Display();
+    }
+}
---
+++ StacksAndQueues/Stacks.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Stacks
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        private Node top;
+        public Stacks()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{ 0} pushed to stack" + " " + value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+}
