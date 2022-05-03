17a03f8cbb425586f2b756dedfca02bee491373c
59114_Chetana <deslechetna1999@gmail.com>
2022-05-03 18:37:49 +0530

    [Chetana]Added:peek and pop from stack till it is empty

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -5,9 +5,39 @@ public class Program
     public static void Main(String[] args)
     {
         Stacks stack = new Stacks();
-        stack.Push(56);
-        stack.Push(30);
-        stack.Push(70);
-        stack.Display();
+        Console.WriteLine("WELCOME IN THE STACK AND QUEUE");
+        bool check = true;
+        Console.WriteLine(" 1. push \n 2. IS_EMPTY \n 3. peek \n 4. pop \n 5. Display \n 6. end");
+        while (check)
+        {
+            Console.WriteLine(" **** ENTER THE ABOVE OPTION TO EXECUTE THE METHODS *****");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Stack");
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+
+                    break;
+                case 2:
+                    stack.IsEmpty();
+                    break;
+                case 3:
+                    stack.Peek();
+                    break;
+                case 4:
+                    stack.Pop();
+                    break;
+                case 5:
+                    stack.Display();
+                    break;
+                case 6:
+                    check = false;
+                    break;
+            }
+        }
+
     }
 }
